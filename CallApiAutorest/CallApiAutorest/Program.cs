f31e620614b9ccafc8d8762313dff38fc428b010
Federico Arambarri <v-fearam@microsoft.com>
2022-01-26 14:50:26 -0300

    Adding some extra POC

--- CallApiAutorest/CallApiAutorest/Program.cs
+++ CallApiAutorest/CallApiAutorest/Program.cs
@@ -1,33 +1,21 @@
-﻿using SampleAPI;
-using System;
+﻿using System;
 using System.Net.Http;
 
 namespace CallApiAutorest
 {
     class Program
     {
-        private const string baseUrl = "http://localhost:21951";
-     
+        private const string baseUrl = "https://petstore.swagger.io/v2";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Autorest");
             HttpClient httpClient = new HttpClient();
             httpClient.BaseAddress = new Uri(baseUrl);
-            var weatherForcast = new WheatherForcast(httpClient, true);
-            var data = weatherForcast.WeatherForecast.GetWithHttpMessagesAsync().Result;
-            foreach (var message in data.Body)
-            {
-                Console.WriteLine($"{message.Summary} {message.TemperatureC} {message.Date}");
-            }
-            Console.WriteLine();
-            Console.WriteLine("NSwag!");
             //NSwag
-            var nswag = new MyServiceClient(httpClient);
-            var response = nswag.GetAsync().Result;
-            foreach (var message in response)
-            {
-                Console.WriteLine($"{message.Summary} {message.TemperatureC} {message.Date}");
-            }
+            var petClient = new PetStoreClient.PetStoreClient(httpClient);
+            var pet = petClient.GetPetByIdAsync(1).Result;
+
+            Console.WriteLine($"{pet.Id} {pet.Name} {pet.Status} {pet.Category.Name}");
         }
     }
 }
---
+++ CustomTask/CustomTask/CallExternalTool.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace CustomTask
+{
+    public class CallExternalTool : ToolTask
+    {
+        [Required]
+        public string InputOpenApi { get; set; }
+        protected override string ToolName => "TestFred";
+
+        protected override string GenerateCommandLineCommands()
+        {
+            //Parametros??
+            return $"openapi2csclient /input:{InputOpenApi}  /classname:PetStoreClient /namespace:PetStoreClient /output:PetStoreClient.cs";
+        }
+
+        protected override string GenerateFullPathToTool()
+        {
+            return "C:\\Users\\far\\Downloads\\Win\\NSwag.exe";
+        }
+
+        protected override bool ValidateParameters()
+        {
+            //absolute path is not allowed
+            LogEventsFromTextOutput($"Test Fred {InputOpenApi} {InputOpenApi.StartsWith("C:\\")}", MessageImportance.High);
+            if (InputOpenApi.StartsWith("C:\\")) 
+                Log.LogError("absolute path is not allowed");
+            return !InputOpenApi.StartsWith("C:\\");
+        }
+    }
+}
